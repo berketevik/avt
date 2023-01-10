2b82d458b86fa1bc1eeaf4b3d762b3b3e4e888cd
Berke Tevik <berketevik1999@gmail.com>
2023-01-10 23:07:24 +0300

    v3

--- pages/_app.js
+++ pages/_app.js
@@ -21,16 +21,16 @@ function MyApp({ Component, pageProps }) {
           href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display"
           rel="stylesheet"
         />
-        <link rel="stylesheet" href={basic} />
+        {/* <link rel="stylesheet" href={basic} />
         <link rel="stylesheet" href={layout} />
         <link rel="stylesheet" href={blogs} />
         <link rel="stylesheet" href={ionicons} />
         <link rel="stylesheet" href={magnific} />
         <link rel="stylesheet" href={animate} />
-        <link rel="stylesheet" href={gradient} />
+        <link rel="stylesheet" href={gradient} /> */}
 
         <title>Aziz Volkan Tevik - CV</title>
-        {/* <link rel="shortcut icon" href="images/favicons/favicon.ico" /> */}
+        <link rel="shortcut icon" href="images/favicons/favicon.ico" />
       </Head>
       <PreLoader />
       <Component {...pageProps} />
